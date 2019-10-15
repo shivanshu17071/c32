40d1dce304f8dd4d66104247ebd8194d62eb0869
shivanshu <shivanshu>
2019-10-15 17:56:21 +0530

    c32

--- sketch.js
+++ sketch.js
@@ -9,9 +9,12 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var bg = "sprites/bg.png"
+
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+	getBackgroundImg();
 }
 
 function setup(){
@@ -45,7 +48,9 @@ function setup(){
 }
 
 function draw(){
+	if(backgroundImg) {
     background(backgroundImg);
+	}
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -69,6 +74,7 @@ function draw(){
     slingshot.display();    
 }
 
+
 function mouseDragged(){
     if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
@@ -85,4 +91,22 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+
+}
+async function getBackgroundImg() {
+      var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+      var responseJSON = await response.json();
+          console.log(responseJSON);
+	  console.log(responseJSON.datetime);
+      var datetime = responseJSON.datetime;
+      var hour = datetime.slice(11,13);
+	  console.log(hour);
+	  if(hour>=06 && hour<=19) {
+		  bg = "sprites/bg.png";
+	  } else {
+		  bg = "sprites/bg2.jpg";
+	  }
+	backgroundImg = loadImage(bg);
+
+	
 }
